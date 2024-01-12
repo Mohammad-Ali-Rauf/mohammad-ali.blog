84fbc248c3a7a270e2169b01240553eb797eebe3
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-12 16:16:03 +0500

    -

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -12,11 +12,26 @@ import { Button } from './ui/button'
 
 // Icons
 import { DashboardIcon, LockOpen1Icon } from '@radix-ui/react-icons'
+import { createBrowserClient } from '@supabase/ssr'
 
 type Props = {}
 
 const Profile = (props: Props) => {
 	const user = useUser((state) => state.user)
+	const setUser = useUser((state) => state.setUser)
+
+	const supabase = createBrowserClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	)
+
+	const handleLogout = () => {
+		supabase.auth.signOut()
+		setUser(undefined)
+	}
+
+	const isAdmin = user?.user_metadata?.role === 'admin'
+
 	return (
 		<Popover>
 			<PopoverTrigger>
@@ -28,17 +43,30 @@ const Profile = (props: Props) => {
 					className='rounded-full ring-2 ring-blue-500'
 				/>
 			</PopoverTrigger>
-			<PopoverContent className='p-2 space-y-3'>
+			<PopoverContent className='p-2 space-y-3 divide-y'>
 				<div className='px-4 text-sm'>
 					<p>{user?.user_metadata?.user_name}</p>
 					<p className='text-gray-500'>{user?.user_metadata?.email}</p>
 				</div>
-				<Link href='/dashboard' className='block'>
-					<Button variant='ghost' className='w-full'>
-            <DashboardIcon className='w-5 h-5 mr-2' />
-						Dashboard
-					</Button>
-				</Link>
+				{isAdmin && (
+					<Link href='/dashboard' className='block'>
+						<Button
+							variant='ghost'
+							className='w-full flex items-center justify-between'
+						>
+							Dashboard
+							<DashboardIcon />
+						</Button>
+					</Link>
+				)}
+				<Button
+					onClick={handleLogout}
+					variant='ghost'
+					className='w-full flex items-center justify-between'
+				>
+					Logout
+					<LockOpen1Icon />
+				</Button>
 			</PopoverContent>
 		</Popover>
 	)
