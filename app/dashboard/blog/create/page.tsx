c2c45daa3cf39b31f0edc7f95ffa0d5c2a17db58
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-13 12:29:51 +0500

    creating the functionality for creating blog in the database

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -7,15 +7,39 @@ import BlogForm from '../../components/BlogForm'
 // Types
 import { BlogFormSchemaType } from '../../schemas'
 
-const page = () => {
+// Functions/Actions
+import { createBlog } from '@/lib/actions/blog'
+import { useToast } from '@/components/ui/use-toast'
 
-	const handleCreate = (data: BlogFormSchemaType) => {
-		console.log(data)
+const Page = () => {
+	const { toast } = useToast()
+	const handleCreate = async (data: BlogFormSchemaType) => {
+		const result = await createBlog(data)
+		const { error } = JSON.parse(result)
+
+		if (error?.message) {
+			toast({
+				title: 'An error occurred while creating the blog.',
+				description: error.message,
+				duration: 1500,
+				variant: 'destructive',
+			})
+		} else {
+			toast({
+				title: 'Successfully created the blog.',
+				description: (
+					<span>
+						The blog with the title of{' '}
+						<span className='font-bold'>{data.title}</span> has been created.
+					</span>
+				),
+				duration: 1500,
+				variant: 'success',
+			})
+		}
 	}
 
-  return (
-	<BlogForm onHandleSubmit={handleCreate} />
-  )
+	return <BlogForm onHandleSubmit={handleCreate} />
 }
 
-export default page
+export default Page
