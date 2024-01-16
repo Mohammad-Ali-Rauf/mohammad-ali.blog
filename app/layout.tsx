dcd524201910f4cbeb3f691f8c1d678f535c74d8
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-16 18:17:31 +0500

    working on it its incomplete yet.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
-// import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/app/providers'
 import Navbar from '@/components/Navbar'
 import SessionProvider from '@/components/session-provider'
 import { Toaster } from '@/components/ui/toaster'
 
-// const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
 	title: 'Your Thoughts, Your Words | ScribeQuantum',
@@ -23,7 +23,7 @@ const RootLayout: React.FC<Props> = ({ children }) => {
 			<head>
 				<link rel='icon' href='/ScribeQuantum.ico' type='image/x-icon' />
 			</head>
-			<body>
+			<body className={inter.className}>
 				<Providers>
 					<main className='max-w-7xl mx-auto p-10 space-y-5'>
 						<Navbar />
