fb792853892ee13976808f7fae39966350d37a9e
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-17 15:46:40 +0500

    -

--- app/blog/[id]/components/Loader.tsx
+++ app/blog/[id]/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loader() {
 	return (
 		<div className="animate-pulse space-y-5">
-			<div className="flex flex-col justify-between items-center">
+			<div className="flex flex-col justify-between items-center gap-y-3">
 				<h1 className=" h-10 w-full bg-graident-dark rounded-md"></h1>
 				<h1 className=" h-10 w-full bg-graident-dark rounded-md"></h1>
 				<h1 className=" h-10 w-full bg-graident-dark rounded-md"></h1>
