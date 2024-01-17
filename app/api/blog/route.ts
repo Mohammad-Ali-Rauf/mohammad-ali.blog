f4d33ad1f25242e39e62f1cebc049910b5279725
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-17 14:31:08 +0500

    -

---
+++ app/api/blog/route.ts
@@ -0,0 +1,25 @@
+// Supabase
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/lib/types/supabase'
+import { NextResponse } from 'next/server'
+
+export async function GET(req: Request) {
+	const supabase = createClient<Database>(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	)
+
+	const { searchParams } = new URL(req.url)
+
+	const id = searchParams.get('id')
+
+	if (id === '*') {
+		const result = await supabase.from('blog').select('id').limit(10)
+
+		return NextResponse.json({ ...result })
+	} else if (id) {
+		const result = await supabase.from('blog').select('*').eq('id', id).single()
+
+		return NextResponse.json({ ...result })
+	}
+}
