f06f1f599c030464775750836f177e690fd0fe84
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-17 15:40:52 +0500

    -

--- app/blog/[id]/components/Loader.tsx
+++ app/blog/[id]/components/Loader.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 export default function Loader() {
 	return (
-		<div className=" animate-pulse space-y-5">
-			<div className="flex justify-between items-center">
+		<div className="animate-pulse space-y-5">
+			<div className="flex flex-col justify-between items-center">
 				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
-				<h1 className=" h-10  w-48 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
+				<h1 className=" h-10 w-56 bg-graident-dark rounded-md"></h1>
 			</div>
 			<div className=" border h-96 rounded-md bg-graident-dark "></div>
 		</div>
