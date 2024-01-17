1bd4899d1bdbf9ce4a577f4e0cbdd5656280eff1
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-17 15:50:14 +0500

    -

--- app/blog/[id]/components/Loader.tsx
+++ app/blog/[id]/components/Loader.tsx
@@ -4,16 +4,16 @@ export default function Loader() {
 	return (
 		<div className="animate-pulse space-y-5">
 			<div className="flex flex-col justify-between items-center gap-y-3">
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
-				<h1 className=" h-10 w-full bg-gray-300 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
+				<h1 className=" h-10 w-full bg-gray-500 rounded-md"></h1>
 			</div>
 			<div className=" border h-96 rounded-md bg-graident-dark "></div>
 		</div>
